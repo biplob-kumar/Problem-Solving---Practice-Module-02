184150115da9c38d05b544b121e0d00e2c0f72af
biplob kumar <biplobkumar125@gmail.com>
2023-02-05 21:10:18 +0600

    Final

--- problem-5/index.js
+++ problem-5/index.js
@@ -8,3 +8,17 @@
 // Test Input:6 and 20
 // Expected Output:“NO”
 // ==========================================>
+
+var  a=10;
+var  a=18;
+
+for(i=20; i<=a && i<=b; i++){
+if( i%a==5 && i%b==10){
+    var g=i;
+console.log(g);
+}
+
+else{
+    console.log("NO");
+}
+}
